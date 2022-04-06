a85a1ea5c7765f64f57c9a4f7e7593e3def6bf1d
Marvin Christian <marvincriss66@gmail.com>
2022-04-06 14:30:55 +0700

    Added mute view and data manager file

---
+++ TimeboxerApp/Data/TimeboxData.swift
@@ -0,0 +1,10 @@
+//
+//  TimeboxData.swift
+//  TimeboxerApp
+//
+//  Created by Marvin Christian on 05/04/22.
+//
+
+import Foundation
+
+var activeTimebox = {}
---
+++ TimeboxerApp/Views/MuteReminderView.swift
@@ -0,0 +1,20 @@
+//
+//  MuteReminderView.swift
+//  TimeboxerApp
+//
+//  Created by Marvin Christian on 05/04/22.
+//
+
+import SwiftUI
+
+struct MuteReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MuteReminderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MuteReminderView()
+    }
+}
