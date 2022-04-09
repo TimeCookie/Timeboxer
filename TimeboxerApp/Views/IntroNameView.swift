0dad894f079e629a6a60c4cbd108ca8e10a5c820
Marvin Christian <marvincriss66@gmail.com>
2022-04-09 14:25:16 +0700

    Finished UI for IntroNameView

--- TimeboxerApp/Views/IntroNameView.swift
+++ TimeboxerApp/Views/IntroNameView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct IntroNameView: View {
+    @State public var userName:String = ""
+    @State public var pass:Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if self.pass {
+                ContentView()
+            }
+            else {
+                Text("What should we call you?")
+                TextField("", text:$userName)
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                    .padding()
+                Divider()
+                 .frame(height: 1)
+                 .background(Color.black)
+                Button(action: {
+                    withAnimation {
+                        self.pass = true
+                    }
+                    
+                }, label: {
+                    Text("Continue")
+                        .bold()
+                        .padding(.vertical)
+                })
+                .padding(.vertical)
+            }
+            
+        }
+        .padding(.horizontal, 50)
     }
 }
 
